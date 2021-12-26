b5d75d43657f69b4018df949eb65e77ecc56d4d8
Aiden <schening@outlook.com>
2021-12-26 20:35:50 +0800

    feature:引入RocketMQ

--- mars-leetcode/src/main/java/com/mars/leetcode/sort/LeetCode347.java
+++ mars-leetcode/src/main/java/com/mars/leetcode/sort/LeetCode347.java
@@ -5,22 +5,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2021/12/22 6:59 下午
+ */
 public class LeetCode347 {
-
-    public int[] topKFrequent(int[] nums, int k) {
-        //使用HashMap存储元素出现频率
+    public static int[] topKFrequent(int[] nums, int k) {
+//使用HashMap存储元素出现频率
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         //桶
-        List<Integer>[] buckets = new List[nums.length + 1];
+        List<Integer>[] buckets = new List[nums.length];
         //往桶里添加元素出现次数
         for (Integer key : map.keySet()) {
             //根据出现频率决定元素入哪个桶
             int count = map.get(key);
             //初始化桶
-            if (buckets[count] == null) buckets[count] = new ArrayList<>();
+            if (buckets[count] == null) {
+                buckets[count] = new ArrayList<>();
+            }
             //将元素存到桶中
             buckets[count].add(key);
         }
@@ -45,7 +51,8 @@ public class LeetCode347 {
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {1, 2};
+        int[] result = topKFrequent(nums, 2);
+        System.out.println(result);
     }
-
 }
