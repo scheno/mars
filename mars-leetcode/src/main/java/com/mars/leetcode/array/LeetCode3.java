d365bbd32b97ced670005c861d4d12c1d640a321
xinghuan <schening@outlook.com>
2021-12-30 23:44:50 +0800

    feature:滑动窗口

---
+++ mars-leetcode/src/main/java/com/mars/leetcode/array/LeetCode3.java
@@ -0,0 +1,33 @@
+package com.mars.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2021/12/30 11:25 下午
+ */
+public class LeetCode3 {
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>(16);
+        int start = 0, end = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            }
+            map.put(c, i);
+            ans = Math.max(ans, end - start + 1);
+            end++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        LeetCode3 leetCode = new LeetCode3();
+        int length = leetCode.lengthOfLongestSubstring("abba");
+        System.out.println(length);
+    }
+}
