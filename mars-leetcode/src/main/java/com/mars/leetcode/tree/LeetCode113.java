a2439afe6d5015c2ae4649d54e61c0e99fece4d6
xinghuan <schening@outlook.com>
2022-01-07 23:24:18 +0800

    feature:二叉树路径和II

---
+++ mars-leetcode/src/main/java/com/mars/leetcode/tree/LeetCode113.java
@@ -0,0 +1,38 @@
+package com.mars.leetcode.tree;
+
+import com.mars.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2022/1/7 11:23 下午
+ */
+public class LeetCode113 {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    Deque<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return result;
+    }
+
+    public void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            result.add(new ArrayList(path));
+        }
+        dfs(root.left, targetSum - root.val);
+        dfs(root.right, targetSum - root.val);
+        path.pollLast();
+    }
+}
