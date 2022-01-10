0e2c7cabb9fe66071c542b26844772e8f865e88c
xinghuan <schening@outlook.com>
2022-01-10 23:38:30 +0800

    feature:二叉树公共祖先

---
+++ mars-leetcode/src/main/java/com/mars/leetcode/tree/ancestor/LeetCode235.java
@@ -0,0 +1,25 @@
+package com.mars.leetcode.tree.ancestor;
+
+import com.mars.leetcode.TreeNode;
+
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2022/1/10 11:37 下午
+ */
+public class LeetCode235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+}
