a2bc49e4d8e7854cccf85d8737c4583d84980249
xinghuan <schening@outlook.com>
2022-01-14 21:56:37 +0800

    feature:重新排列

---
+++ mars-leetcode/src/main/java/com/mars/leetcode/array/arrange/LeetCode31.java
@@ -0,0 +1,46 @@
+package com.mars.leetcode.array.arrange;
+
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2022/1/14 4:46 下午
+ */
+public class LeetCode31 {
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1);
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int i) {
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 2};
+        nextPermutation(nums);
+    }
+
+}
