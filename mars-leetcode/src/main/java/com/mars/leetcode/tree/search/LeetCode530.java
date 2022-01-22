df849ed3e114ee38e269c6f742595d39b41eb02d
xinghuan <schening@outlook.com>
2022-01-22 23:38:33 +0800

    feature:最小绝对差

--- mars-leetcode/src/main/java/com/mars/leetcode/tree/search/LeetCode530.java
+++ mars-leetcode/src/main/java/com/mars/leetcode/tree/search/LeetCode530.java
@@ -14,20 +14,16 @@ public class LeetCode530 {
     Integer res = Integer.MAX_VALUE;
 
     public int getMinimumDifference(TreeNode root) {
-        dfs(root);
-        return res;
-    }
-
-    public void dfs(TreeNode root) {
         if (root == null) {
-            return;
+            return 0;
         }
-        dfs(root.left);
+        getMinimumDifference(root.left);
         if (pre != null) {
             res = Math.min(res, root.val - pre.val);
         }
         pre = root;
-        dfs(root.right);
+        getMinimumDifference(root.right);
+        return res;
     }
 
 }
