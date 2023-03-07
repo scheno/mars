341aac29ce32c7fe11ddbc2f7eaa81445e42bf37
xinghuan <schening@outlook.com>
2023-03-08 00:16:02 +0800

    feat(leetcode): # 全排列 # 相交链表

--- mars-leetcode/src/main/java/com/mars/leetcode/backtracking/LeetCode46.java
+++ mars-leetcode/src/main/java/com/mars/leetcode/backtracking/LeetCode46.java
@@ -1,47 +1,42 @@
 package com.mars.leetcode.backtracking;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+
 import java.util.List;
 
 /**
+ * 全排列
+ * 经典回溯算法
+ * 由于是全排列，则每次遍历不需要指定起始位置
+ * 由于数据不能存在重复，需要定义used数组对已使用数据做标记
+ *
  * @author shenchen
  * @version 1.0
  * @date 2021/12/29 10:40 下午
  */
 public class LeetCode46 {
 
-    List<List<Integer>> result;
-
-    LinkedList<Integer> path;
-
-    boolean[] used;
-
     public List<List<Integer>> permute(int[] nums) {
-        result = new ArrayList<>();
-        path = new LinkedList<>();
-        if (nums == null || nums.length == 0) {
-            result.add(null);
-        }
-        backtrack(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        int[] used = new int[nums.length];
+        backtracking(nums, used, path, result);
         return result;
     }
 
-    public void backtrack(int[] nums) {
+    public void backtracking(int[] nums, int[] used, List<Integer> path, List<List<Integer>> result) {
         if (path.size() == nums.length) {
-            result.add(new LinkedList<>(path));
-            return;
+            result.add(new ArrayList<>(path));
         }
-
         for (int i = 0; i < nums.length; i++) {
-            if (used[i] == true) {
+            if (used[i] == 1) {
                 continue;
             }
-            used[i] = true;
-            path.addLast(nums[i]);
-            backtrack(nums);
-            path.removeLast();
-            used[i] = false;
+            path.add(nums[i]);
+            used[i] = 1;
+            backtracking(nums, used, path, result);
+            path.remove(path.size() - 1);
+            used[i] = 0;
         }
     }
 
--- mars-leetcode/src/main/java/com/mars/leetcode/linkedlist/itersection/LeetCode160.java
+++ mars-leetcode/src/main/java/com/mars/leetcode/linkedlist/itersection/LeetCode160.java
@@ -3,6 +3,11 @@ package com.mars.leetcode.linkedlist.itersection;
 import com.mars.leetcode.ListNode;
 
 /**
+ * 相交链表
+ * 长度分别为 a、b、c
+ * 那 a + c + b = b + c + a
+ *
+ *
  * @author shenchen
  * @version 1.0
  * @date 2022/1/18 7:54 下午
@@ -13,17 +18,23 @@ public class LeetCode160 {
         ListNode first = headA;
         ListNode second = headB;
         while (first != null && second != null) {
+            // 如果节点形同，则直接返回
             if (first == second) {
                 break;
             }
+            // 分别指向下一个节点
             first = first.next;
             second = second.next;
+            // 如果第一个遍历完则指向另外一个
             if (first == null) {
                 first = headB;
+                // 将另一个置为空，防止出现死循环
                 headB = null;
             }
+            // 如果第一个遍历完则指向另外一个
             if (second == null) {
                 second = headA;
+                // 将另一个置为空，防止出现死循环
                 headA = null;
             }
         }
