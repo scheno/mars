9a948d8f59ed1b442df795b0afdf96e6a4726a51
沈晨 <schening@outlook.com>
2023-04-05 00:05:18 +0800

    hashtable

---
+++ mars-leetcode/src/main/java/com/mars/leetcode/hashtable/LeetCode1.java
@@ -0,0 +1,28 @@
+package com.mars.leetcode.hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * LeetCode1.两数之和
+ *
+ * 该题除了暴力解法之外，可以利用Map缓存已经被遍历过的数值及对应下标，
+ * 当target-nums[i]的值在map中存在，则就找到了对应下标位置
+ *
+ * @author shenchen
+ * @version 1.0
+ * @date 2022/1/17 3:33 下午
+ */
+public class LeetCode1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) return new int[] {map.get(x), i};
+            map.put(nums[i], i);
+        }
+        return new int[] {-1 ,-1};
+    }
+
+}
---
+++ mars-leetcode/src/main/java/com/mars/leetcode/hashtable/LeetCode1002.java
@@ -0,0 +1,18 @@
+package com.mars.leetcode.hashtable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author shenchen
+ * @version 1.0
+ * @date 2021/12/27 9:51 下午
+ */
+public class LeetCode1002 {
+
+    public List<String> commonChars(String[] words) {
+        return null;
+    }
+
+}
