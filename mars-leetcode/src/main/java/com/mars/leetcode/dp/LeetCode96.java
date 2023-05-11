d422b721d60963d5ac20142749a828b0e626ad70
沈晨 <schening@outlook.com>
2023-05-12 00:09:54 +0800

    动态规划：不同的二叉搜索树

--- mars-leetcode/src/main/java/com/mars/leetcode/dp/LeetCode96.java
+++ mars-leetcode/src/main/java/com/mars/leetcode/dp/LeetCode96.java
@@ -15,7 +15,7 @@ public class LeetCode96 {
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i - 1; j++) {
+            for (int j = 1; j <= i; j++) {
                 // 对于第i个节点，需要考虑1作为根节点直到i作为根节点的情况，所以需要累加
                 // 一共i个节点，对于根节点j时,左子树的节点个数为j-1，右子树的节点个数为i-j
                 dp[i] += dp[j - 1] * dp[i - j];
